7d9de67883ad592d510b6ce692a0492b17942a09
virshu <felix@rabinovich.org>
2022-12-28 17:17:11 -0800

    Ugly styles (Listing 2.7)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 class Slider extends HTMLElement {
   connectedCallback() {
-    alert('hi from Slider');        1
+    this.innerHTML = '<div class="bg-overlay"></div><div class="thumb"></div>';
+    this.style.display = 'inline-block';
+    this.style.position = 'relative';
+    this.style.width = '500px';
+    this.style.height = '50px';
+
+    this.querySelector('.bg-overlay').style.width = '100%';
+    this.querySelector('.bg-overlay').style.height = '100%';
+    this.querySelector('.bg-overlay').style.position = 'absolute';
+    this.querySelector('.bg-overlay').style.backgroundColor = this.getAttribute('backgroundcolor') || 'red';
+
+    this.querySelector('.thumb').style.marginLeft = '100px';
+    this.querySelector('.thumb').style.width = '5px';
+    this.querySelector('.thumb').style.height = 'calc(100% - 5px)';
+    this.querySelector('.thumb').style.position = 'absolute';
+    this.querySelector('.thumb').style.border = '3px solid white';
+    this.querySelector('.thumb').style.borderRadius = '3px';
   }
 }
 if (!customElements.get('wcia-slider')) {
